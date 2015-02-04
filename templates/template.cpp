87a766f92b31ca6b961ee4b2c793a2f2a4066db5
Ilya Mitin <rumterg@gmail.com>
2015-02-04 14:20:31 +0300

    add templates

---
+++ templates/template.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <utility>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <list>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+
+#define forn(i, n) for (int i = 0; i < (int)(n); ++ i)
+#define forv(it, v) for (it = (v).begin (); it != (v).end (); ++ it)
+#define all(v) (v).begin (), (v).end ()
+#define sqr(x) ((x) * (x))
+#define ll long long
+
+using namespace std;
+
+class 
+{
+public:
+	
+	{
+	}
+};
